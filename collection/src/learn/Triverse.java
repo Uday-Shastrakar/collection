6e22cfd4e029ccf4c01b81d4b957a23cf7f58de4
Uday Shastrakar <uday.shastrakar@gmail.com>
2022-12-30 23:01:51 +0530

    map

--- collection/src/learn/Triverse.java
+++ collection/src/learn/Triverse.java
@@ -1,19 +1,71 @@
 package learn;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Triverse {
     public static void main(String[] args) {
-        ArrayList<String> name =new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
         name.add("uday");
         name.add("shubham");
         name.add("mohit");
         name.add("uday");
 
         //for each loop
-        for(String str:name){
-            System.out.println(str +"\t"+str.length());
+        for (String str : name) {
+            System.out.print(str + "\t" + str.length() + "\t");
+            StringBuffer br = new StringBuffer(str);
+            System.out.println(br.reverse());
         }
+        System.out.println("---------------------");
+
+        //eversing using iterator forward treverse
+        Iterator<String> itr = name.iterator();
+        while (itr.hasNext()) {
+            String next = itr.next();
+            System.out.println(next);
+        }
+        System.out.println("_____________________");
+        //backwared treversal of collection
+        ListIterator<String> litr = name.listIterator(name.size());
+        while (litr.hasPrevious()) {
+            String previous = litr.previous();
+            System.out.println(previous);
+        }
+        System.out.println("______________");
+        //Enumeration
+
+        Vector v=new Vector();
+        v.addAll(name);
+        v.add("shubham");
+
+        Enumeration e= v.elements();
+        while(e.hasMoreElements()){
+            Object o = e.nextElement();
+            System.out.println(o);
+        }
+        System.out.println("_________________-for Each--------------------");
+        //for each method
+        name.forEach(f->{System.out.println(f);});
+
+        System.out.println("------Treeset---sorting of elements-----");
+
+        TreeSet<String> set =new TreeSet<>();
+        set.addAll(name);
+        set.forEach(f ->{
+            System.out.println(f);
+        });
+
+        //comaprable
+//        comparator
+
+
+
+
 
     }
+
+
+
+
+
 }
---
+++ collection/src/learn/mapex.java
@@ -0,0 +1,7 @@
+package learn;
+
+public class mapex {
+    public static void main(String[] args) {
+        
+    }
+}
