30eb381cfb227e6d2643a5faa2059eabe8227f58
Uday Shastrakar <uday.shastrakar@gmail.com>
2022-12-31 09:23:43 +0530

    map

--- collection/src/learn/mapex.java
+++ collection/src/learn/mapex.java
@@ -1,7 +1,21 @@
 package learn;
 
+import java.util.HashMap;
+
 public class mapex {
     public static void main(String[] args) {
-        
+        HashMap<String,Integer> courses= new HashMap<>();
+        //adding element
+        courses.put("core java",4000);
+        courses.put("python",3500);
+        courses.put("spring",8000);
+
+
+        System.out.println(courses);
+
+        courses.forEach((e1,e2)->{
+            System.out.println(e1+"=>" + e2);
+        });
+
     }
 }
