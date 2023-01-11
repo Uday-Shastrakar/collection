ca9cc5680397bad13ed42d51aca792f2f95cdc2b
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-01-11 19:03:50 +0530

    initial

--- collection/src/loops/pattern.java
+++ collection/src/loops/pattern.java
@@ -4,6 +4,12 @@ public class pattern {
 
     public static void main(String[] args) {
         System.out.println("hi");
+         for(int i=0;i<10;i++){
+             for(int y=0;y<10;y++){
+                 System.out.println("hi");
+             }
+         }
+
     }
 
 }
